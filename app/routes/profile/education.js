e1552fc28c425a512c51f30c4a1926a843edfe37
Thomas Wang <wangxgwxg@gmail.com>
2020-10-07 17:26:48 -0700

    fix "Cannot access OldEducationRoute before initialization"

--- app/routes/profile/education.js
+++ app/routes/profile/education.js
@@ -1,15 +1,16 @@
 import Route from '@ember/routing/route';
 import { setupFlaggedRoute } from './../../lib/flag-route';
 
-export default setupFlaggedRoute(OldEducationRoute, {
-  flagKey: 'education',
-  enabledRouteName: 'profile/new-education'
-});
-
-class OldEducationRoute extends Route {
-  model() {
-    return {
-      message: 'This is the old education page'
-    };
+export default setupFlaggedRoute(
+  class OldEducationRoute extends Route {
+    model() {
+      return {
+        message: 'This is the old education page',
+      };
+    }
+  },
+  {
+    flagKey: 'education',
+    enabledRouteName: 'profile/new-education',
   }
-}
+);
