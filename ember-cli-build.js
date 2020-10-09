49ae7d7067f8cc6935672eb03f2f936f9a0dbd5b
Thomas Wang <wangxgwxg@gmail.com>
2020-10-09 16:07:05 -0700

    Classic build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,7 +20,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  if (process.env.CLASSIC) {
+  if (!process.env.EMBROIDER) {
     return app.toTree();
   }
 
