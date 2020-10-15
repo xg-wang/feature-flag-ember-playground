83ae3716406e3f807ebf1ce76e5708512c5f653f
Thomas Wang <wangxgwxg@gmail.com>
2020-10-14 21:00:26 -0700

    Fix async route waiting
    Use promise chains to properly wait

--- app/services/flag.js
+++ app/services/flag.js
@@ -1,26 +1,29 @@
 import Service, { inject as service } from '@ember/service';
-import { all } from 'rsvp';
 
 export default Service.extend({
   router: service('router'),
   init() {
     this._super(...arguments);
-    this.ROUTE_FLAGS_MAP = new Map([
-      [
-        FLAGS,
-        new Map([
-          [TOP_LEVEL_FLAGS, new Map()],
-          [INHERIT_FLAGS, new Map()],
-        ]),
-      ],
-    ]);
+    const topLevelFlagsManager = createFlagsManager();
+    topLevelFlagsManager.resolve(
+      new Map([
+        [TOP_LEVEL_FLAGS, new Map()],
+        [INHERIT_FLAGS, new Map()],
+      ])
+    );
+    this.ROUTE_FLAGS_MAP = new Map([[FLAGS, topLevelFlagsManager]]);
     this.router.on('routeWillChange', ({ from, to, routeInfos }) => {
       const fromList = createList(from);
       const toList = createList(to);
       const { pivot } = diffRoutes({ fromList, toList });
       let routePromises = routeInfos.map((info) => info.routePromise);
       // application -> profile -> connections
-      updateFlagsMapForRouteHierarchy(this.ROUTE_FLAGS_MAP, routePromises, pivot);
+      updateFlagsMapForRouteHierarchy(
+        this.ROUTE_FLAGS_MAP,
+        toList,
+        routePromises,
+        pivot
+      );
     });
   },
   // when get-route-info lands we can drop private API usage
@@ -40,27 +43,37 @@ export default Service.extend({
 
 async function updateFlagsMapForRouteHierarchy(
   routeFlagsMap,
+  toList,
   routePromises,
   pivot
 ) {
   // init flags map for each route hierarchy
+  let initPtr = routeFlagsMap;
+  for (const { localName } of toList) {
+    if (!initPtr.has(localName)) {
+      initPtr.set(
+        localName,
+        new Map([
+          [PARENT, initPtr],
+          [FLAGS, createFlagsManager()],
+        ])
+      );
+    }
+    initPtr = initPtr.get(localName);
+  }
+  // resolve the flags for routes
   let ptr = routeFlagsMap;
   for (let i = 0; i < routePromises.length; i++) {
-    // TODO: this is relying on routerjs resolves model in a later micro stack, we can win the race :)
-    // a more reliable approach is to have snapshot wait.
-    await routePromises[i].then((route) => {
-      console.log(`[async route resolve] resolved ${route.fullRouteName}`);
-      const localName = routeLocalName(route.fullRouteName);
-      if (!ptr.has(localName)) {
-        ptr.set(localName, new Map([[PARENT, ptr]]));
-      }
-      ptr = ptr.get(localName);
-      // TODO: figure out if we can use buildRouteInfoMetadata
-      const { flags } = route;
+    // TODO: figure out if we can use buildRouteInfoMetadata to get flags
+    const { flags: flagEvaluator, fullRouteName } = await routePromises[i];
+    console.log(`[async route resolve] resolved ${fullRouteName}`);
+    const { localName } = toList[i];
+    ptr = ptr.get(localName);
+    if (flagEvaluator) {
       // flags.keys: ['a', 'b']
-      if (flags && !ptr.has(FLAGS)) {
-        const parentFlagsMap = ptr.get(PARENT).get(FLAGS);
-        const topLevelFlagsEntries = flags.keys
+      if (!ptr.get(FLAGS).valueExists()) {
+        const parentFlagsMap = ptr.get(PARENT).get(FLAGS).valueSync();
+        const topLevelFlagsEntries = flagEvaluator.keys
           .filter(
             (f) =>
               !parentFlagsMap.get(TOP_LEVEL_FLAGS).has(f) &&
@@ -74,19 +87,24 @@ async function updateFlagsMapForRouteHierarchy(
           [TOP_LEVEL_FLAGS, new Map(topLevelFlagsEntries)],
           [INHERIT_FLAGS, new Map(inheritFlagsEntries)],
         ]);
-        ptr.set(FLAGS, flagsMapForThisLevel);
-        FLAGS_TO_MAP.set(flags, ptr);
+        ptr.get(FLAGS).resolve(flagsMapForThisLevel);
+        FLAGS_EVALUATOR_TO_MAP.set(flagEvaluator, ptr);
+        // tell evaluator snapshot can be resolved now, this is to allow using
+        // async snapshot in model hooks where the async route also needs be
+        // waited to init in routeWillChange
+        flagEvaluator._resolve();
       }
       // update flags for routes that are below pivot
       if (i >= pivot) {
-        if (flags) {
-          const ptr = FLAGS_TO_MAP.get(flags);
-          for (const v of ptr.get(FLAGS).get(TOP_LEVEL_FLAGS).values()) {
-            v.update();
-          }
+        for (const v of ptr
+          .get(FLAGS)
+          .valueSync()
+          .get(TOP_LEVEL_FLAGS)
+          .values()) {
+          v.update();
         }
       }
-    });
+    }
   }
 }
 
@@ -103,6 +121,7 @@ class FlagRef {
   }
 }
 
+// Operate global flags cache
 const STORE = new Map(
   Object.entries(
     JSON.parse(
@@ -110,20 +129,16 @@ const STORE = new Map(
     ).flags
   )
 );
-
 export function getFlag(key) {
   return STORE.get(key) || 'control';
 }
-
 export function getFlagIsEnabled(key) {
   return getFlag(key) !== 'control';
 }
-
 export function updateFlag(key) {
   const newValue = STORE.get(key) + 1;
   STORE.set(key, newValue);
 }
-
 export function setFlag(key, value) {
   STORE.set(key, value);
 }
@@ -133,33 +148,76 @@ const FLAGS = '.flags';
 const TOP_LEVEL_FLAGS = '.TOP_LEVEL_FLAGS';
 const INHERIT_FLAGS = '.INHERIT_FLAGS';
 
-const FLAGS_TO_MAP = new WeakMap();
-
-// TODO: make evaluation tracked?
+const FLAGS_EVALUATOR_TO_MAP = new WeakMap();
 export class FlagEvaluator {
+  _resolver;
+  _pending;
+  _activePromiseChain;
   constructor(keys) {
     this.keys = keys;
+    this._activePromiseChain = new Promise((r) => (this._resolver = r));
   }
   _snapshotSync() {
-    const flagsMap = FLAGS_TO_MAP.get(this).get(FLAGS);
-    const snapshot = Object.create(null);
-    for (let key of this.keys) {
-      const value = flagsMap.get(TOP_LEVEL_FLAGS).has(key)
-        ? flagsMap.get(TOP_LEVEL_FLAGS).get(key).value
-        : flagsMap.get(INHERIT_FLAGS).get(key).value;
-      Object.defineProperty(snapshot, key, {
-        get() {
-          notifyEvaluation({ key, value });
-          return value;
-        },
-        enumerable: true,
-      });
-    }
-    return snapshot;
+    const flagsMap = FLAGS_EVALUATOR_TO_MAP.get(this).get(FLAGS).valueSync();
+    return snapshotFromFlagsMap(flagsMap, this.keys);
+  }
+  _resolve() {
+    this._resolver();
   }
   snapshot() {
-    return Promise.resolve(this._snapshotSync());
+    return this._activePromiseChain.then(() =>
+      FLAGS_EVALUATOR_TO_MAP.get(this)
+        .get(FLAGS)
+        .value()
+        .then((flagsMap) => snapshotFromFlagsMap(flagsMap, this.keys))
+    );
   }
+}
+function snapshotFromFlagsMap(flagsMap, keys) {
+  const snapshot = Object.create(null);
+  for (let key of keys) {
+    const value = flagsMap.get(TOP_LEVEL_FLAGS).has(key)
+      ? flagsMap.get(TOP_LEVEL_FLAGS).get(key).value
+      : flagsMap.get(INHERIT_FLAGS).get(key).value;
+    Object.defineProperty(snapshot, key, {
+      get() {
+        notifyEvaluation({ key, value });
+        return value;
+      },
+      enumerable: true,
+    });
+  }
+  return snapshot;
+}
+
+function createFlagsManager() {
+  let pending = true;
+  let v;
+  let resolver;
+  let activePromiseChain = new Promise((resolve) => (resolver = resolve));
+  return {
+    valueExists() {
+      return typeof v !== 'undefined';
+    },
+    value() {
+      if (pending === true) {
+        return activePromiseChain;
+      } else {
+        return Promise.resolve(v);
+      }
+    },
+    valueSync() {
+      return v;
+    },
+    resolve(resolvedValue) {
+      if (resolver) {
+        resolver(resolvedValue);
+      }
+      pending = false;
+      v = resolvedValue;
+      activePromiseChain = undefined;
+    },
+  };
 }
 
 // Utils
@@ -167,6 +225,7 @@ function notifyEvaluation({ key, value }) {
   // sendBeacon('/feature-falg-analytics')
   console.log(`${key} evaled as ${value}`);
 }
+
 function createList(routeInfo) {
   const ret = [];
   if (routeInfo === null) {
@@ -186,17 +245,24 @@ function diffRoutes({ fromList, toList }) {
     if (!toSegment || !fromSegment) {
       return { pivot: i };
     }
-    const { name: toName, params: toParams } = toList[i];
-    const { name: fromName, params: fromParams } = fromList[i];
-    if (toName !== fromName || !objectsMatch(toParams, fromParams))
+    const { name: toName, localName: toLocalName, params: toParams } = toList[
+      i
+    ];
+    const {
+      name: fromName,
+      localName: fromLocalName,
+      params: fromParams,
+    } = fromList[i];
+    // /profile/2 -> /profile/2/connections does not trigger model()
+    if (
+      (toName !== fromName &&
+        toLocalName !== 'index' &&
+        fromLocalName !== 'index') ||
+      !objectsMatch(toParams, fromParams)
+    )
       return { pivot: i };
   }
   return { pivot: toList.length };
-}
-
-function routeLocalName(routeName) {
-  let parts = routeName.split('.');
-  return parts[parts.length - 1];
 }
 
 function objectsMatch(obj1, obj2) {
